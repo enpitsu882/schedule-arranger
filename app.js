3d3e0d86b3e04e7d873279fb52743e7508b81a80
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-04-06 20:17:08 +0900

    GitHub認証を実装

--- app.js
+++ app.js
@@ -3,11 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
+
+const GitHubStrategy = require('passport-github2').Strategy;
+const GITHUB_CLIENT_ID ='126b427d0eb59c48ffd8';
+const GITHUB_CLIENT_SECRET ='ed75e175b8236dbc13a98597212a9a8474f24ffe';
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    process.nextTick(() => {
+      return done(null, profile);
+    });
+  }
+));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +48,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: '7ba6e89311b72907', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }),
+  (req, res) => {}
+);
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failuerRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
