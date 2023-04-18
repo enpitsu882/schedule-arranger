2bcb8141f2a3be42c16c68018cfbc73fb242428c
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-04-18 18:07:49 +0900

    ログイン時のリダイレクト機能の実装

--- app.js
+++ app.js
@@ -91,7 +91,14 @@ app.get('/auth/github',
 app.get('/auth/github/callback',
   passport.authenticate('github', { failuerRedirect: '/login' }),
   (req, res) => {
-    res.redirect('/');
+    const loginFrom = req.cookies.loginFrom;
+    // オープンリダイレクタ脆弱性対策
+    if (loginFrom && loginFrom.startsWith('/')) {
+      res.clearCookie('loginFrom');
+      res.redirect(loginFrom);
+    } else {
+      res.redirect('/');
+    }
   }
 );
 
